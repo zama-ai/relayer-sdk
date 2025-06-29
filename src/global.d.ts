a9654650ce0580d4fb623fda9c816fa12fef01ab
Clément Danjou <clement@danjou.io>
2025-06-30 00:13:51 +0200

    fix: add missing export (#271)
    * fix: add missing export
    
    * 0.1.0-8

--- src/global.d.ts
+++ src/global.d.ts
@@ -16,7 +16,18 @@ declare global {
     CompactCiphertextList: any;
     ZkComputeLoad: any;
   };
-  type TKMS = any;
+  type TKMS = {
+    default?: any;
+    u8vec_to_ml_kem_pke_pk: any;
+    u8vec_to_ml_kem_pke_sk: any;
+    new_client: any;
+    new_server_id_addr: any;
+    process_user_decryption_resp_from_js: any;
+    ml_kem_pke_keygen: any;
+    ml_kem_pke_pk_to_u8vec: any;
+    ml_kem_pke_sk_to_u8vec: any;
+    ml_kem_pke_get_pk: any;
+  };
   var TFHE: TFHE;
   var TKMS: TKMS;
 }
