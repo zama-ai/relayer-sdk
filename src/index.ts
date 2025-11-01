b52c8462a43065ee68632645b9e521e67f805a06
0xalexbel <alexandre.belhoste@zama.ai>
2025-11-01 08:23:23 +0100

    chore: new addresses and gateway chainid

--- src/index.ts
+++ src/index.ts
@@ -78,21 +78,21 @@ export type FhevmInstance = {
 
 export const SepoliaConfig: FhevmInstanceConfig = {
   // ACL_CONTRACT_ADDRESS (FHEVM Host chain)
-  aclContractAddress: '0x687820221192C5B662b25367F70076A37bc79b6c',
+  aclContractAddress: '0xf0Ffdc93b7E186bC2f8CB3dAA75D86d1930A433D',
   // KMS_VERIFIER_CONTRACT_ADDRESS (FHEVM Host chain)
-  kmsContractAddress: '0x1364cBBf2cDF5032C47d8226a6f6FBD2AFCDacAC',
+  kmsContractAddress: '0xbE0E383937d564D7FF0BC3b46c51f0bF8d5C311A',
   // INPUT_VERIFIER_CONTRACT_ADDRESS (FHEVM Host chain)
-  inputVerifierContractAddress: '0xbc91f3daD1A5F19F8390c400196e58073B6a0BC4',
+  inputVerifierContractAddress: '0xBBC1fFCdc7C316aAAd72E807D9b0272BE8F84DA0',
   // DECRYPTION_ADDRESS (Gateway chain)
   verifyingContractAddressDecryption:
-    '0xb6E160B1ff80D67Bfe90A85eE06Ce0A2613607D1',
+    '0x5D8BD78e2ea6bbE41f26dFe9fdaEAa349e077478',
   // INPUT_VERIFICATION_ADDRESS (Gateway chain)
   verifyingContractAddressInputVerification:
-    '0x7048C39f048125eDa9d678AEbaDfB22F7900a29F',
+    '0x483b9dE06E4E4C7D35CCf5837A1668487406D955',
   // FHEVM Host chain id
   chainId: 11155111,
   // Gateway chain id
-  gatewayChainId: 55815,
+  gatewayChainId: 10901,
   // Optional RPC provider to host chain
   network: 'https://eth-sepolia.public.blastapi.io',
   // Relayer URL
