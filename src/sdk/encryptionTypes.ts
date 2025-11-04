3bec945d8255a52b905d634affebf695c5037721
0xalexbel <alexandre.belhoste@zama.ai>
2025-11-04 17:40:06 +0100

    fix: prettier

--- src/sdk/encryptionTypes.ts
+++ src/sdk/encryptionTypes.ts
@@ -21,12 +21,12 @@
  * | 256  | 8         | `euint256`      | The maximum supported integer size. |
  */
 export const ENCRYPTION_TYPES = {
-  2: 0,   // ebool (FheTypeId=0) is using 2 encrypted bits 
+  2: 0, // ebool (FheTypeId=0) is using 2 encrypted bits
   // euint4 (FheTypeId=1) is deprecated
-  8: 2,   // euint8 (FheTypeId=2) is using 8 encrypted bits
-  16: 3,  // euint16 (FheTypeId=3) is using 16 encrypted bits
-  32: 4,  // euint32 (FheTypeId=4) is using 32 encrypted bits
-  64: 5,  // euint64 (FheTypeId=5) is using 64 encrypted bits
+  8: 2, // euint8 (FheTypeId=2) is using 8 encrypted bits
+  16: 3, // euint16 (FheTypeId=3) is using 16 encrypted bits
+  32: 4, // euint32 (FheTypeId=4) is using 32 encrypted bits
+  64: 5, // euint64 (FheTypeId=5) is using 64 encrypted bits
   128: 6, // euint128 (FheTypeId=128) is using 128 encrypted bits
   160: 7, // euint160/eaddress (FheTypeId=7) is using 160 encrypted bits
   256: 8, // euint256 (FheTypeId=8) is using 256 encrypted bits
