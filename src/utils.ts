05026421055fe563c5743ca9514a0ac9cf7a2116
0xalexbel <alexandre.belhoste@zama.ai>
2025-11-19 13:51:49 +0100

    chore: new utils functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
+import {
+  isAddress as ethersIsAddress,
+  getAddress as ethersGetAddress,
+} from 'ethers';
+
+export type ChecksummedAddress = `0x${string}`;
+
 export const SERIALIZED_SIZE_LIMIT_CIPHERTEXT = BigInt(1024 * 1024 * 512);
 export const SERIALIZED_SIZE_LIMIT_PK = BigInt(1024 * 1024 * 512);
 export const SERIALIZED_SIZE_LIMIT_CRS = BigInt(1024 * 1024 * 512);
@@ -48,4 +55,77 @@ export const bytesToBigInt = function (byteArray: Uint8Array): bigint {
 
 export function ensure0x(s: string): `0x${string}` {
   return !s.startsWith('0x') ? `0x${s}` : (s as `0x${string}`);
+}
+
+export function isChecksummedAddress(
+  value: unknown,
+): value is ChecksummedAddress {
+  if (typeof value !== 'string') {
+    return false;
+  }
+  const a = ethersGetAddress(value);
+  return a === value;
+}
+
+export function assertIsChecksummedAddress(
+  value: unknown,
+): asserts value is ChecksummedAddress {
+  if (!isChecksummedAddress(value)) {
+    throw new TypeError('Invalid Checksummed Address');
+  }
+}
+
+export function isAddress(value: unknown): value is `0x${string}` {
+  return ethersIsAddress(value);
+}
+
+export function assertIsAddress(
+  value: unknown,
+): asserts value is `0x${string}` {
+  if (!isAddress(value)) {
+    throw new TypeError('Invalid Address');
+  }
+}
+
+export function isBytesHex(value: unknown): value is `0x${string}` {
+  if (typeof value !== 'string') {
+    return false;
+  }
+  if (!value.startsWith('0x')) {
+    return false;
+  }
+  const hexRegex = /^0x[a-fA-F0-9]*$/;
+  if (!hexRegex.test(value)) {
+    return false;
+  }
+  if ((value.length - 2) % 2 !== 0) {
+    return false;
+  }
+  return true;
+}
+
+export function isBytes32Hex(value: unknown): value is `0x${string}` {
+  if (!isBytesHex(value)) {
+    return false;
+  }
+  if (value.length !== 66) {
+    throw new RangeError('Invalid Bytes32Hex');
+  }
+  return true;
+}
+
+export function assertIsBytesHex(
+  value: unknown,
+): asserts value is `0x${string}` {
+  if (!isBytesHex(value)) {
+    throw new RangeError('Invalid BytesHex');
+  }
+}
+
+export function assertIsBytes32Hex(
+  value: unknown,
+): asserts value is `0x${string}` {
+  if (!isBytes32Hex(value)) {
+    throw new RangeError('Invalid Bytes32Hex');
+  }
 }
