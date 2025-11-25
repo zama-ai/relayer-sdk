037660292610fab1a6f1649218f53c3354000938
0xalexbel <alexandre.belhoste@zama.ai>
2025-11-25 16:59:11 +0100

    fix: abiEncodeClearValues with euint256

--- src/relayer/publicDecrypt.ts
+++ src/relayer/publicDecrypt.ts
@@ -93,8 +93,7 @@ function abiEncodeClearValues(clearValues: ClearValues) {
       case 4: //euint32
       case 5: //euint64
       case 6: //euint128
-      case 7: {
-        //euint256
+      case 8: { //euint256
         // bigint
         abiValues.push(clearTextValueBigInt);
         break;
