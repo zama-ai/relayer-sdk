0876763ced2e796e66594da1cee626f525b0bbc5
0xalexbel <alexandre.belhoste@zama.ai>
2026-01-02 22:08:44 +0100

    chore: prettier

--- src/_version.ts
+++ src/_version.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated
-export const version: string = "0.4.0-alpha.4";
-export const sdkName: string = "@zama-fhe/relayer-sdk";
+export const version: string = '0.4.0-alpha.4';
+export const sdkName: string = '@zama-fhe/relayer-sdk';
